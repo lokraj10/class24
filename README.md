8dcb7672021e30e53e724d792e702a6ff2a03df8
lokraj10 <89073251+lokraj10@users.noreply.github.com>
2021-09-28 20:21:44 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# class24
+# PRO-C24-Reference_code
+reference code
---
+++ js/boat.js
@@ -0,0 +1,18 @@
+class Boat{
+    constructor(x,y,width,height,boatPos ){
+        this.body = Bodies.rectangle(x,y,width,height);
+        this.width = width;
+        this.height = height;
+        this.boatPosition = boatPos;
+        this.image = loadImage("assets/boat.png")
+        World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position
+        push();
+        translate(pos.x,pos.y)
+        imageMode(CENTER)
+        image(this.image,0,this.boatPosition,this.width,this.height)
+        pop();
+    }
+}
